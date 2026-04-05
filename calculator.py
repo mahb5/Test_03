5d893b1968fafc78c47e2686a13b188a132282ed
mahb5 <marcoshermosa@gmail.com>
2026-04-05 11:26:33 -0300

    Add a division function.

--- calculator.py
+++ calculator.py
@@ -2,4 +2,12 @@ n1 = int(input('Enter the first number: '))
 n2 = int(input('Enter the second number: '))
 
 print(f'The sum of the two numbers is {n1 + n2}.')
+
 print(f'The result of multiplying {n1} and {n2} is {n1 * n2}.')
+
+if n2 != 0:
+    print(f'The result of dividing {n1} by {n2} is {n1 / n2}.')
+else:
+    print('Cannot divide by zero.')
+
+
