95c9d30b99fc8c85ce4a96b0c5023bc9a3d466c1
mahb5 <marcoshermosa@gmail.com>
2026-04-05 11:21:45 -0300

    Add a multiplication function.

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 n1 = int(input('Enter the first number: '))
 n2 = int(input('Enter the second number: '))
 
-print(f'The sum of the two numbers is {n1+n2}.')
+print(f'The sum of the two numbers is {n1 + n2}.')
+print(f'The result of multiplying {n1} and {n2} is {n1 * n2}.')
