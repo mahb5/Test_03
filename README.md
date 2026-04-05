c3ba0ec634c40a470664985d8d3036e4ef5102f6
mahb5 <marcoshermosa@gmail.com>
2026-04-05 10:18:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple Python-based application designed to practice Python and learn how to use GitHub.
---
+++ calculator.py
@@ -0,0 +1,4 @@
+n1 = int(input('Enter the first number: '))
+n2 = int(input('Enter the second number: '))
+
+print(f'The sum of the two numbers is {n1+n2}.')
